6e4d5bf867603108603ae5e5178a56f7d0048db3
prathambhansali <prathambhansali10@gmail.com>
2022-07-15 17:47:43 +0530

    migrations

--- drawtool/models.py
+++ drawtool/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from datetime import datetime
-from project_module.models import Project
+from project_module.models import Project,ProjectProcessedData
 
+# Create your models here.
 measures = (
     ('line', 'line'),
     ('poly', 'poly'),
@@ -11,10 +12,11 @@ measures = (
 
 class AOI(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    date = models.CharField(max_length=10)
+    # date = models.ForeignKey(ProjectProcessedData, on_delete=models.CASCADE)
+    date=models.CharField(max_length=10)
     label = models.CharField(max_length=255, default="Undefined")
     description = models.CharField(max_length=255, default="")
-    creation_date = models.DateTimeField(default=datetime.now())
+    creation_date=models.DateTimeField(default=datetime.now())
     polygon = models.TextField(default=[], null=True, blank=True)
 
     def __str__(self):
@@ -26,8 +28,8 @@ class Measure(models.Model):
     type = models.CharField(max_length=20, choices=measures, default='')
     label = models.CharField(max_length=255, default="")
     description = models.CharField(max_length=255, default="")
-    creation_date = models.DateTimeField(default=datetime.now())
-    measurements = models.CharField(max_length=255, default="")
+    creation_date=models.DateTimeField(default=datetime.now())
+    measurements = models.CharField(max_length=255,default="")
     polygon = models.TextField(default={}, null=True, blank=True)
 
     def __str__(self):
